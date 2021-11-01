efa679120de077a590497595cff2ecdd15fb2a7b
kjoos22 <kjoos22@gmail.com>
2021-11-01 12:48:12 -0400

    JS solution

---
+++ first-bad-version/JavaScriptSolution.js
@@ -0,0 +1,36 @@
+
+//Definition for isBadVersion()
+
+//@param {integer} version number
+//@return {boolean} whether the version is bad
+//isBadVersion = function(version) {
+//  ...
+//};
+///
+
+
+//@param {function} isBadVersion()
+//@return {function}
+
+
+//https://leetcode.com/problems/first-bad-version/submissions/
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        first = 1
+        last = n
+        
+        while (first < last) {
+            mid = Math.floor(first + (last - first) / 2)
+            if (isBadVersion(mid))
+                last = mid
+            else
+                first = mid + 1
+        }
+        
+        return first
+    };
+};
