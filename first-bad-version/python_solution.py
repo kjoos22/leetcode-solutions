0790dcd04ad70d4f46dfa73fde1c486386795aa3
kjoos22 <kjoos22@gmail.com>
2021-11-01 12:56:27 -0400

    python solution

---
+++ first-bad-version/python_solution.py
@@ -0,0 +1,28 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+# https://leetcode.com/problems/first-bad-version/
+
+import math
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        first = 1
+        last = n
+        
+        while first < last:
+            mid = math.trunc(math.floor(first + ((last - first) / 2)))
+            if isBadVersion(mid):
+                last = mid
+            else:
+                first = mid + 1
+                
+        return first
+        
