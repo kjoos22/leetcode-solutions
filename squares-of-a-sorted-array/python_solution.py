63297ba37eec59dd2d77a340dfd6a9d7791f658b
kjoos22 <kjoos22@gmail.com>
2021-11-17 11:33:09 -0500

    Python solution

---
+++ squares-of-a-sorted-array/python_solution.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/squares-of-a-sorted-array/
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        i = 0
+        while i < len(nums):
+            nums[i] = nums[i] * nums[i]
+            i += 1
+        
+        nums.sort()
+        
+        return nums
