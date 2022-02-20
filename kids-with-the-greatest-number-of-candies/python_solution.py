37c7ce3113031043399c140dfaa8bd9f26821f0f
kjoos22 <kjoos22@gmail.com>
2022-02-20 13:34:58 -0500

    python solution

---
+++ kids-with-the-greatest-number-of-candies/python_solution.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        most_candies = max(candies)
+        have_most_candies = []
+        for candy in candies:
+            if candy + extraCandies >= most_candies:
+                have_most_candies.append(True)
+            else:
+                have_most_candies.append(False)
+        return have_most_candies
